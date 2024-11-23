0f7b8744597591a4a9ad87e0b849e3d5989adea0
Videomapper <guillermofce@gmail.com>
2024-11-23 08:42:10 -0600

    Update gallery.js

--- assets/scripts/gallery.js
+++ assets/scripts/gallery.js
@@ -1,13 +1,13 @@
-document.querySelector('.gallery-title').addEventListener('mouseover', function() {
+document.querySelector('.gallery-title').addEventListener('mouseover', function () {
     let images = document.querySelectorAll('.gallery-image');
-    images.forEach(function(image) {
+    images.forEach(function (image) {
         image.style.opacity = 1;
     });
 });
 
-document.querySelector('.gallery-title').addEventListener('mouseout', function() {
+document.querySelector('.gallery-title').addEventListener('mouseout', function () {
     let images = document.querySelectorAll('.gallery-image');
-    images.forEach(function(image) {
+    images.forEach(function (image) {
         image.style.opacity = 0;
     });
 });
