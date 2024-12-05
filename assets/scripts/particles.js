9ac12e82e6af5211042909662b79f23be32f4999
Videomapper <guillermofce@gmail.com>
2024-12-05 13:59:41 -0600

    Delete assets/scripts/particles.js

--- assets/scripts/particles.js
+++
@@ -1,6 +0,0 @@
-// Añadir contenedor para partículas al cargar la página
-document.addEventListener('DOMContentLoaded', () => {
-    if (!document.querySelector('.particles-container')) {
-        document.body.insertAdjacentHTML('beforeend', '<div class="particles-container"></div>');
-    }
-});
